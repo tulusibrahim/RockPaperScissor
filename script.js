962b076d1067e39ed5542fa950e3b8b2e83ebf90
tulusibrahim <tulusibrahim20@gmail.com>
2020-09-19 09:45:35 +0700

    fixing some bugs so it more interactive

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
+var you = ["gunting", "batu", "kertas"];
+
+function kertas() {
+  document.getElementById("you").innerHTML = "kertas"
+  you = "kertas"
+}
+function gunting() {
+  document.getElementById("you").innerHTML = "gunting"
+  you = "gunting"
+}
+function batu() {
+  document.getElementById("you").innerHTML = "batu"
+  you = "batu"
+}
+
+
 let scoreyou = 0;
 let scorebot = 0;
 
 function yuk() {
   var judgement = document.getElementById("judgement");
 
-  var you = ["gunting", "batu", "kertas"];
-  var outputyou = you[Math.floor(Math.random() * you.length)];
-  document.getElementById("you").innerHTML = outputyou;
-
   var bot = ["gunting", "batu", "kertas"];
   var outputbot = bot[Math.floor(Math.random() * bot.length)];
   document.getElementById("bot").innerHTML = outputbot;
 
   let jawaban =
-    outputyou == "gunting" && outputbot == "gunting"
+    you == "gunting" && outputbot == "gunting"
       ? "seri"
-      : outputyou == "gunting" && outputbot == "batu"
-      ? "kalah"
-      : outputyou == "gunting" && outputbot == "kertas"
-      ? "menang"
-      : outputyou == "batu" && outputbot == "gunting"
-      ? "menang"
-      : outputyou == "batu" && outputbot == "batu"
-      ? "seri"
-      : outputyou == "batu" && outputbot == "kertas"
-      ? "kalah"
-      : outputyou == "kertas" && outputbot == "gunting"
-      ? "kalah"
-      : outputyou == "kertas" && outputbot == "batu"
-      ? "menang"
-      : outputyou == "kertas" && outputbot == "kertas"
-      ? "seri"
-      : "";
+      : you == "gunting" && outputbot == "batu"
+        ? "kalah"
+        : you == "gunting" && outputbot == "kertas"
+          ? "menang"
+          : you == "batu" && outputbot == "gunting"
+            ? "menang"
+            : you == "batu" && outputbot == "batu"
+              ? "seri"
+              : you == "batu" && outputbot == "kertas"
+                ? "kalah"
+                : you == "kertas" && outputbot == "gunting"
+                  ? "kalah"
+                  : you == "kertas" && outputbot == "batu"
+                    ? "menang"
+                    : you == "kertas" && outputbot == "kertas"
+                      ? "seri"
+                      : "";
   judgement.innerHTML = jawaban;
 
   if (jawaban == "menang") {
